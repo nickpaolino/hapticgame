990f374c8edd3b50bb634742cefc4e7dd4c6eda3
Nick Paolino <github email address>
2017-11-11 16:49:58 -0500

    Initiated Rails Project

---
+++ app/controllers/message_controller.rb
@@ -0,0 +1,12 @@
+class MessageController < ApplicationController
+  def create
+    @message = Message.create(content: params[:content])
+
+    render json: @message
+  end
+
+  def index
+    @messages = Message.all
+    render json: @messages
+  end
+end
